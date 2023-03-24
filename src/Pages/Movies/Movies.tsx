4268e3839baa0d22fdb6b596b7ef4163fd99f4b1
Do Hai <dohaizxc@gmail.com>
2023-03-24 21:27:00 +0700

    update news and offer

--- src/Pages/Movies/Movies.tsx
+++ src/Pages/Movies/Movies.tsx
@@ -46,78 +46,81 @@ export const Movies = () => {
 
   return (
     <Layout>
-      <Tabs
-        setSelectedTab={setSelectedType}
-        tab1="PHIM ĐANG CHIẾU"
-        tab2="PHIM SẮP CHIẾU"
-      ></Tabs>
-      <div className="flex flex-col items-center justify-center lg:mx-12 sm:mx-5 mx-0 mb-5">
-        {isLoading ? (
-          <div className="flex justify-center min-h-screen">
-            <Spin size="large" tip="Loading..." />
-          </div>
-        ) : (
-          <div className="flex flex-wrap justify-center gap-x-5 gap-y-5">
-            {movies?.map((movie: Movie) => (
-              <div
-                key={movie._id}
-                className="sm:w-[192px] w-[170px] sm:h-[455px] h-[420px] relative"
-              >
-                {movie.rated.substring(0, 1) === "P" ? (
-                  <p className="absolute top-2 left-2 bg-green-500 rounded-full font-semibold text-white py-1 px-[11px] z-10">
-                    P
-                  </p>
-                ) : (
-                  <p className="absolute top-2 left-2 bg-red-500 rounded-full font-semibold text-white p-1 z-10">
-                    {movie.rated.substring(0, 3)}
-                  </p>
-                )}
+      <div className="bg-white">
+        <Tabs
+          setSelectedTab={setSelectedType}
+          tab1="PHIM ĐANG CHIẾU"
+          tab2="PHIM SẮP CHIẾU"
+        ></Tabs>
 
-                <img
-                  className="sm:w-[192px] sm:h-[276px] w-[170px] h-[240px] mx-auto cursor-pointer transition ease-in-out delay-150 hover:scale-105 duration-300 rounded"
-                  src={movie.image}
-                  alt={movie.name}
-                  onClick={() => {
-                    scroll(0, 0);
-                    navigate(`/movie/${movie._id}`);
-                  }}
-                ></img>
+        <div className="flex justify-center pb-5">
+          {isLoading ? (
+            <div className="flex justify-center min-h-screen">
+              <Spin size="large" tip="Loading..." />
+            </div>
+          ) : (
+            <div className="grid 2xl:grid-cols-6 lg:grid-cols-5 min-[840px]:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-x-5 gap-y-5">
+              {movies?.map((movie: Movie) => (
                 <div
-                  className="line-clamp-2 cursor-pointer font-bold mt-3 hover:text-sky-500"
-                  onClick={() => {
-                    scroll(0, 0);
-                    navigate(`/movie/${movie._id}`);
-                  }}
+                  key={movie._id}
+                  className="sm:w-[192px] w-[170px] sm:h-[455px] h-[420px] relative"
                 >
-                  {movie.name}
-                </div>
-                <div>
-                  <span className="font-medium">Thể loại: </span>
-                  {movie.genre.join(", ")}
-                </div>
-                <div>
-                  <span className="font-medium">Thời lượng: </span>
-                  {movie.duration} phút
-                </div>
-                <div>
-                  <span className="font-medium">Khởi chiếu: </span>
-                  {new Date(movie.releaseDate).toLocaleDateString("en-UK")}
-                </div>
-                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
-                  <button
-                    className="px-4 py-2 border border-transparent rounded-md font-semibold text-white bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:shadow-outline-blue transition duration-150 ease-in-out"
+                  {movie.rated.substring(0, 1) === "P" ? (
+                    <p className="absolute top-2 left-2 bg-green-500 rounded-full font-semibold text-white py-1 px-[11px] z-10">
+                      P
+                    </p>
+                  ) : (
+                    <p className="absolute top-2 left-2 bg-red-500 rounded-full font-semibold text-white p-1 z-10">
+                      {movie.rated.substring(0, 3)}
+                    </p>
+                  )}
+
+                  <img
+                    className="sm:w-[192px] sm:h-[276px] w-[170px] h-[240px] mx-auto cursor-pointer transition ease-in-out delay-150 hover:scale-105 duration-300 rounded"
+                    src={movie.image}
+                    alt={movie.name}
+                    onClick={() => {
+                      scroll(0, 0);
+                      navigate(`/movie/${movie._id}`);
+                    }}
+                  ></img>
+                  <div
+                    className="line-clamp-2 cursor-pointer font-bold mt-3 hover:text-sky-500"
                     onClick={() => {
                       scroll(0, 0);
                       navigate(`/movie/${movie._id}`);
                     }}
                   >
-                    {selectedType ? <>Mua vé</> : <>Chi tiết</>}
-                  </button>
+                    {movie.name}
+                  </div>
+                  <div>
+                    <span className="font-medium">Thể loại: </span>
+                    {movie.genre.join(", ")}
+                  </div>
+                  <div>
+                    <span className="font-medium">Thời lượng: </span>
+                    {movie.duration} phút
+                  </div>
+                  <div>
+                    <span className="font-medium">Khởi chiếu: </span>
+                    {new Date(movie.releaseDate).toLocaleDateString("en-UK")}
+                  </div>
+                  <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
+                    <button
+                      className="px-4 py-2 border border-transparent rounded-md font-semibold text-white bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:shadow-outline-blue transition duration-150 ease-in-out"
+                      onClick={() => {
+                        scroll(0, 0);
+                        navigate(`/movie/${movie._id}`);
+                      }}
+                    >
+                      {selectedType ? <>Mua vé</> : <>Chi tiết</>}
+                    </button>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </Layout>
   );
--- src/Pages/NewsOffer/NewsOffer.tsx
+++ src/Pages/NewsOffer/NewsOffer.tsx
@@ -29,44 +29,50 @@ export const NewsOffer = () => {
 
   return (
     <Layout>
-      <Tabs setSelectedTab={setSelectedTab} tab1="ƯU ĐÃI" tab2="TIN MỚI"></Tabs>
+      <div className="bg-white">
+        <Tabs
+          setSelectedTab={setSelectedTab}
+          tab1="ƯU ĐÃI"
+          tab2="TIN MỚI"
+        ></Tabs>
 
-      {isLoading ? (
-        <div className="flex justify-center min-h-screen">
-          <Spin size="large" tip="Loading..." />
-        </div>
-      ) : (
-        <div className="flex flex-wrap justify-center lg:gap-y-10 gap-y-5 lg:gap-x-10 gap-x-5 mb-10">
-          {newsOffers?.map((newsOffer: NewsOfferInterface) => {
-            return (
-              <div className="md:w-60 sm:w-50 w-40">
-                <img
-                  className="rounded transition ease-in-out delay-150 hover:scale-105 duration-300"
-                  src={newsOffer.img}
-                  alt={newsOffer.name}
-                  onError={({ currentTarget }) => {
-                    currentTarget.onerror = null;
-                    currentTarget.src = errImage;
-                  }}
-                />
-                <h1
-                  className="sm:h-12 h-10 line-clamp-2 sm:text-sm text-xs text-center font-semibold uppercase cursor-pointer pt-2 hover:text-sky-500"
-                  onClick={() => {
-                    scroll(0, 0);
-                    navigate(`/newsoffer/detail/${newsOffer.id}`);
-                  }}
-                >
-                  {newsOffer.name}
-                </h1>
-                <div className="flex sm:space-x-2 space-x-1 items-center pt-1 sm:px-2 pb-2">
-                  <CalendarDaysIcon className="sm:h-6 sm:w-6 h-4 w-4" />
-                  <p className="sm:text-sm text-xs">{newsOffer.date}</p>
+        {isLoading ? (
+          <div className="flex justify-center min-h-screen">
+            <Spin size="large" tip="Loading..." />
+          </div>
+        ) : (
+          <div className="flex flex-wrap justify-center lg:gap-y-10 gap-y-5 lg:gap-x-10 gap-x-5 mb-10">
+            {newsOffers?.map((newsOffer: NewsOfferInterface) => {
+              return (
+                <div className="md:w-60 sm:w-50 w-40">
+                  <img
+                    className="rounded transition ease-in-out delay-150 hover:scale-105 duration-300"
+                    src={newsOffer.img}
+                    alt={newsOffer.name}
+                    onError={({ currentTarget }) => {
+                      currentTarget.onerror = null;
+                      currentTarget.src = errImage;
+                    }}
+                  />
+                  <h1
+                    className="sm:h-12 h-10 line-clamp-2 sm:text-sm text-xs text-center font-semibold uppercase cursor-pointer pt-2 hover:text-sky-500"
+                    onClick={() => {
+                      scroll(0, 0);
+                      navigate(`/newsoffer/detail/${newsOffer.id}`);
+                    }}
+                  >
+                    {newsOffer.name}
+                  </h1>
+                  <div className="flex sm:space-x-2 space-x-1 items-center pt-1 sm:px-2 pb-2">
+                    <CalendarDaysIcon className="sm:h-6 sm:w-6 h-4 w-4" />
+                    <p className="sm:text-sm text-xs">{newsOffer.date}</p>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-      )}
+              );
+            })}
+          </div>
+        )}
+      </div>
     </Layout>
   );
 };
