331628a588f5f1ce0febc984206af243f8a628f7
Do Hai <dohaizxc@gmail.com>
2023-03-25 15:00:26 +0700

    update notfound page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Button, Result } from "antd";
-import { Link } from "react-router-dom";
 
 export const NotFound = () => {
   return (
     <main className="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">404</p>
+        <p className="text-6xl font-bold text-sky-600">404</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           Page not found
         </h1>
@@ -16,11 +14,11 @@ export const NotFound = () => {
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <a
             href="/"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-sky-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go back home
           </a>
-          <a href="#" className="text-sm font-semibold text-gray-900">
+          <a href="/" className="text-sm font-semibold text-gray-900">
             Contact support <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
