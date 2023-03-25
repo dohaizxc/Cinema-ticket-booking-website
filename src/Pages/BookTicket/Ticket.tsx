a0c1e443be06f9bc35e71f2a08f17e0273f24158
Do Hai <dohaizxc@gmail.com>
2023-03-25 12:02:57 +0700

    update ticket

--- src/Pages/BookTicket/Ticket.tsx
+++ src/Pages/BookTicket/Ticket.tsx
@@ -10,7 +10,6 @@ import {
   BuildingOfficeIcon,
 } from "@heroicons/react/24/outline";
 import QRCode from "qrcode.react";
-import { Steps } from "../../components/Steps";
 
 export const Ticket = () => {
   const navigate = useNavigate();
@@ -25,11 +24,11 @@ export const Ticket = () => {
   return (
     <Layout>
       {ticket && (
-        <div className="flex flex-col items-center sm:mx-20 mx-10 my-5 space-y-4 min-h-screen">
+        <div className="flex flex-col items-center sm:mx-20 mx-5 my-5 space-y-4 min-h-screen">
           <h1 className="font-bold text-lg bg-sky-300 py-2 w-full text-center rounded">
             THÔNG TIN VÉ
           </h1>
-          <div className="flex lg:flex-row flex-col lg:justify-between lg:space-x-16 bg-sky-100 py-5 sm:px-10 px-8 lg:h-[400px] w-full">
+          <div className="flex lg:flex-row flex-col lg:justify-between lg:space-x-16 bg-sky-100 py-5 sm:px-10 px-6 lg:h-[400px] w-full">
             <img
               src={ticket.movieImage}
               className="rounded sm:w-[240px] sm:h-[350px] h-[300px] w-fit mt-2 mx-auto"
@@ -78,7 +77,7 @@ export const Ticket = () => {
                   <div>
                     <div>BẮP NƯỚC:</div>
 
-                    <div className="ml-10">
+                    <div className="sm:ml-10 ml-4">
                       {ticket.foods?.map((food: Food) => {
                         return (
                           <div
@@ -105,7 +104,7 @@ export const Ticket = () => {
                 )}
 
                 <div className="w-full flex justify-between">
-                  <span className="ml-0">TỔNG THANH TOÁN:</span>
+                  <span>TỔNG THANH TOÁN:</span>
                   <span>
                     {(
                       (ticket.totalTicket ?? 0) + (ticket.totalFood ?? 0)
@@ -117,7 +116,7 @@ export const Ticket = () => {
                 </div>
 
                 <div className="w-full flex justify-between">
-                  <span className="sm:mr-10 mr-2">PHƯƠNG THỨC THANH TOÁN:</span>
+                  <span className="sm:mr-10">PHƯƠNG THỨC THANH TOÁN:</span>
                   <span className="font-normal">{ticket.paymentMethod}</span>
                 </div>
               </div>
