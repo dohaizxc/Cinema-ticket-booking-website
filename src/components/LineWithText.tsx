3e9647b6fc38a85e8e69ffda2f5fc4cd5c8a949d
Do Hai <dohaizxc@gmail.com>
2023-03-28 12:46:36 +0700

    update LineWithText

--- src/components/LineWithText.tsx
+++ src/components/LineWithText.tsx
@@ -14,7 +14,7 @@ export const LineWithText = (props: {
   return (
     <div className="relative h-16">
       <div className="absolute bottom-[50%] right-[50%] translate-x-[50%] translate-y-[50%] w-full h-1 bg-gray-600"></div>
-      <div className="absolute bottom-[50%] right-[50%] translate-x-[50%] translate-y-[50%] bg-gray-100 px-4">
+      <div className="absolute bottom-[50%] right-[50%] translate-x-[50%] translate-y-[50%] bg-gray-100 sm:px-4 px-2">
         <span className="font-bold text-lg sm:text-xl lg:text-2xl text-blue-900">
           {props.children}
         </span>
