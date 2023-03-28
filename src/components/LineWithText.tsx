fdca0fbc742463ec072a5b86d3f987e6461ea2de
Do Hai <dohaizxc@gmail.com>
2023-03-28 12:40:17 +0700

    update LineWithText

--- src/components/LineWithText.tsx
+++ src/components/LineWithText.tsx
@@ -12,14 +12,13 @@ export const LineWithText = (props: {
     | undefined;
 }) => {
   return (
-    <div className="flex justify-center items-center h-16">
-      <div className="w-1/3 sm:w-1/4 lg:w-1/2 h-1 bg-gray-600"></div>
-      <div className="text-center sm:w-1/3 w-7/12">
+    <div className="relative h-16">
+      <div className="absolute bottom-[50%] right-[50%] translate-x-[50%] translate-y-[50%] w-full h-1 bg-gray-600"></div>
+      <div className="absolute bottom-[50%] right-[50%] translate-x-[50%] translate-y-[50%] bg-gray-100 px-4">
         <span className="font-bold text-lg sm:text-xl lg:text-2xl text-blue-900">
           {props.children}
         </span>
       </div>
-      <div className="w-1/3 sm:w-1/4 lg:w-1/2 h-1 bg-gray-600"></div>
     </div>
   );
 };
