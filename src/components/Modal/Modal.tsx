938626f1da430ed895c12f72647d62c10a3196c4
Do Hai <dohaizxc@gmail.com>
2023-04-07 13:07:54 +0700

    update darkmode modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -48,26 +48,39 @@ export const Modal: React.FC<{
               leaveFrom="opacity-100 translate-y-0 scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white dark:bg-slate-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-sky-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <div className="mx-auto flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full bg-sky-100 sm:mx-0 sm:h-10 sm:w-10">
                       <InformationCircleIcon
-                        className="h-6 w-6 text-sky-600"
+                        className="h-5 w-5 sm:h-6 sm:w-6 text-sky-600"
                         aria-hidden="true"
                       />
                     </div>
-                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
-                        {title}
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">{content}</p>
+                    {content ? (
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900 dark:text-white"
+                        >
+                          {title}
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <p className="text-sm text-gray-500 dark:text-white">
+                            {content}
+                          </p>
+                        </div>
                       </div>
-                    </div>
+                    ) : (
+                      <div className="pt-2 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900 dark:text-white"
+                        >
+                          {title}
+                        </Dialog.Title>
+                      </div>
+                    )}
                   </div>
                 </div>
                 <div className="px-4 pb-4 sm:flex sm:flex-row-reverse sm:px-6">
@@ -83,7 +96,7 @@ export const Modal: React.FC<{
                   </button>
                   <button
                     type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    className="mt-3 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-200 dark:hover:bg-slate-600 dark:text-white sm:mt-0 sm:w-auto"
                     onClick={() => setOpenModal(false)}
                     ref={cancelButtonRef}
                   >
